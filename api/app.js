4ee06c7203a95e9ee1bae45e2b7f55627d981f9c
Raghav6761 <45282779+Raghav6761@users.noreply.github.com>
2022-11-24 22:02:29 +0530

    1.1
    data extracted, handling pending

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 console.log('api folder imdex sayms Hemloooo!');
+const http = require('http');
 const path = require("path");
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -8,7 +9,7 @@ const app = express();
 
 const fs = require('fs');
 const { parse }= require('csv-parse');
-const assert = require('assert');
+// const assert = require('assert');
 
 
 const MIME_TYPE_MAP = {
@@ -50,44 +51,108 @@ app.use("/backend",express.static(path.join("backend")));
 
 // app.post('/api/test',multer({storage: storage}).single("fila"), (req,res,next)=>{
 app.post('/api/test',multer({storage: storage}).fields([{name: 'source', maxCount: 1},{name: 'mapping', maxCount: 1}]), (req,res,next)=>{
-    let records = [];
-
     const url = req.protocol + '://' + req.get("host");;
     const sourceUrl = url + '/backend/inputJson/' + req.files['source'][0].filename;
     const mappingUrl = url + '/backend/mapping/' + req.files['mapping'][0].filename;
     
-    console.log(req.files['mapping'][0]);
+    // const sourceJson = require('../backend/inputJson'+req.files['source'][0].filename);
+    // const sourceJson = require('/backend/inputJson'+req.files['source'][0].filename);
+    let records = [];
+    // console.log('sourceJson ',sourceJson);
+
     // console.log(sourceUrl, mappingUrl);
 
     // const readPath = path.join(__dirname, '../', 'backend/mapping/mapping.csv_converted1669294541264.csv');
     const readPath = path.join(__dirname, '../', 'backend/mapping/'+req.files['mapping'][0].filename);
-    
     console.log(readPath);
+
     const parser = parse({columns: true, relax_quotes:true, relax_column_count: true}, function (err, records) {
-        console.log('err : ',err);
+        if (err){
+            console.log('err : ',err);
+            res.status(201).json({
+                // message: 'post req sent',
+                // report : report
+                message: 'records could not be read',
+                output : err
+            });
+        }
         this.records = records;
         
-        console.log('records have been changed', this.records);
-        res.status(201).json({
-            // message: 'post req sent',
-            // report : report
-            message: 'records have been altered',
-            output : records
-        })    
+        // console.log('records have been changed', this.records);
+        // res.status(201).json({
+        //     // message: 'post req sent',
+        //     // report : report
+        //     message: 'records have been altered',
+        //     output : records
+        // });
+        
+        let jsonReq = http.get(sourceUrl, function(response) {
+            let data = '',
+                json_data;
+
+            response.on('data', function(stream) {
+                data += stream;
+            });
+            response.on('end', function() {
+                json_data = JSON.parse(data);
+
+                // output_data = {};
+                let outputArr = [];
+
+                // Code to iterate through the array
+                let lenRecord = records.length;
+                // let lenLine = records[0].length;
+                for (let i = 0;i < lenRecord; i++){
+                    console.log('line number',i+1, records[i]);
+                    console.log(records[i]['Target']);
+                    console.log(records[i][' Source']);
+                    console.log(records[i][' Enumeration']);
+
+                    let arr1 = records[i][' Source'].split(" + ");
+
+                    console.log('before for loop', arr1);
+                    for (let j = 0; j < arr1.length; j++){
+                        arr1[j] = arr1[j].trim();
+                        console.log(arr1[j]);
+                        if(arr1[j].substring(0,5)=='ENUM('){
+                            console.log('it has enum');
+                            arr2 = arr1[j].split('.');
+                            for(let k=1; k<arr2.length; k++){
+                                arr2[k] = arr2[k].trim;
+                                console.log('json data at this point',json_data.arr2[k]);
+                                
+                            }
+                        }else{
+                            console.log('not enum');
+                        }
+                    }
+
+                }
+                // Code to iterate through the array - Complete
+
+                // will output a Javascript object
+                // console.log(json_data);
+                res.status(201).json({
+                    message: 'records have been altered',
+                    records : records,
+                    sourceJson : json_data
+                });
+            });
+        });
+
+        jsonReq.on('error', function(e) {
+            console.log(e.message);
+        });
     });
     fs.createReadStream(readPath).pipe(parser);
-    
-    // parse(readPath,{trim: true, skip_empty_lines: true, columns: true})
-    
-    console.log('records go here' , records);
 
-    const report = [
-        {
-            dirname : readPath,
-            sourceUrl : sourceUrl,
-            mappingUrl : mappingUrl
-        }
-    ];
+    // const report = [
+    //     {
+    //         dirname : readPath,
+    //         sourceUrl : sourceUrl,
+    //         mappingUrl : mappingUrl
+    //     }
+    // ];
 })
 
 app.get('/api/test',(req,res,next)=>{
