b7b2310a1438c2ce3893676c155356889f2c7de4
Raghav6761 <45282779+Raghav6761@users.noreply.github.com>
2022-11-24 19:26:50 +0530

    1.0
    reading CSV file for mapping complete

--- api/app.js
+++ api/app.js
@@ -1,9 +1,15 @@
 console.log('api folder imdex sayms Hemloooo!');
+const path = require("path");
 const express = require('express');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 
 const app = express();
+
+const fs = require('fs');
+const { parse }= require('csv-parse');
+const assert = require('assert');
+
 
 const MIME_TYPE_MAP = {
     'application/json' : 'json',
@@ -34,6 +40,7 @@ const storage = multer.diskStorage({
 
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(bodyParser.json());
+app.use("/backend",express.static(path.join("backend")));
 
 // app.use((res,req,next)=>{
 //     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -43,23 +50,44 @@ app.use(bodyParser.json());
 
 // app.post('/api/test',multer({storage: storage}).single("fila"), (req,res,next)=>{
 app.post('/api/test',multer({storage: storage}).fields([{name: 'source', maxCount: 1},{name: 'mapping', maxCount: 1}]), (req,res,next)=>{
-    console.log('in the post test');
-    console.log(req.body);
-    console.log('complete request',req);
+    let records = [];
+
+    const url = req.protocol + '://' + req.get("host");;
+    const sourceUrl = url + '/backend/inputJson/' + req.files['source'][0].filename;
+    const mappingUrl = url + '/backend/mapping/' + req.files['mapping'][0].filename;
+    
+    console.log(req.files['mapping'][0]);
+    // console.log(sourceUrl, mappingUrl);
+
+    // const readPath = path.join(__dirname, '../', 'backend/mapping/mapping.csv_converted1669294541264.csv');
+    const readPath = path.join(__dirname, '../', 'backend/mapping/'+req.files['mapping'][0].filename);
+    
+    console.log(readPath);
+    const parser = parse({columns: true, relax_quotes:true, relax_column_count: true}, function (err, records) {
+        console.log('err : ',err);
+        this.records = records;
+        
+        console.log('records have been changed', this.records);
+        res.status(201).json({
+            // message: 'post req sent',
+            // report : report
+            message: 'records have been altered',
+            output : records
+        })    
+    });
+    fs.createReadStream(readPath).pipe(parser);
+    
+    // parse(readPath,{trim: true, skip_empty_lines: true, columns: true})
+    
+    console.log('records go here' , records);
+
     const report = [
         {
-            id : req.body.id,
-            name: req.body.name,
-            status: req.body.status,
-            source : req.files.source,
-            mapping : req.files.mapping
+            dirname : readPath,
+            sourceUrl : sourceUrl,
+            mappingUrl : mappingUrl
         }
     ];
-
-    res.status(201).json({
-        message: 'post req sent',
-        report : report
-    })
 })
 
 app.get('/api/test',(req,res,next)=>{
@@ -71,9 +99,7 @@ app.get('/api/test',(req,res,next)=>{
         }
     ]
 
-    console.log('before send')
-    // res.send.json(testStruct);
-    // res.send('returing');
+    console.log('before send');
 
     res.status(200).json({
         message: 'struct Sent!',
